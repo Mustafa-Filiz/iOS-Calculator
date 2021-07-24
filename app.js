fc7379529edabf64b4840dd2c1325afb14719e87
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-07-24 14:02:59 +0300

    html and css added

---
+++ app.js
@@ -0,0 +1,73 @@
+//Variables
+
+const previousElement = document.querySelector(".previous-result");
+const currentElement = document.querySelector(".current-result");
+
+
+const acButton = document.querySelector(".ac");
+const posnegButton = document.querySelector(".posneg");
+const modButton = document.querySelector(".mod");
+
+const additionButton = document.querySelector(".plus");
+const subtractionButton = document.querySelector(".minus");
+const divisionButton = document.querySelector(".division");
+const multiplicationButton = document.querySelector(".multiply");
+const equalButton = document.querySelector(".equal");
+
+const commaButton = document.querySelector(".comma");
+const number0 = document.querySelector(".number-0");
+const number1 = document.querySelector(".number-1");
+const number2 = document.querySelector(".number-2");
+const number3 = document.querySelector(".number-3");
+const number4 = document.querySelector(".number-4");
+const number5 = document.querySelector(".number-5");
+const number6 = document.querySelector(".number-6");
+const number7 = document.querySelector(".number-7");
+const number8 = document.querySelector(".number-8");
+const number9 = document.querySelector(".number-9");
+
+const numbersArray = [
+    number0,
+    number1,
+    number2,
+    number3,
+    number4,
+    number5,
+    number6,
+    number7,
+    number8,
+    number9
+]
+
+let previousOperand = "";
+let currentOperand = "";
+
+//Functions
+
+function displayNumbers() {
+    previousElement.innerHTML = previousOperand;
+    currentElement.innerHTML = currentOperand;
+}
+
+function appendNumber(number) {
+    previousOperand.
+}
+
+//Add event listener to operator buttons
+
+
+// Add event listener to top buttons
+
+
+// Add event listener to number buttons
+
+for (let i = 0; i < numbersArray.length; i++) {
+    const number = numbersArray[i];
+
+    number.addEventListener("click", () => {
+        console.log("NUMBER:", i);
+        appendNumber(i);
+
+    })
+    
+}
